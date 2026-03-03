677d604705c3dfb7929ae4ee5fea216e118be68c
vsuraev <vadim.suraev@gmail.com>
2026-03-03 21:57:54 +0000

    paths fixed in code generation script

--- tools/run_parser.sh
+++ tools/run_parser.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-project_root=../
+project_root=./
 header_file_path=
-if [ $# -lt 2 ];then
+if [ $# -lt 1 ];then
 	echo "need at least a header file path"
 	exit 1
 fi
@@ -10,4 +10,4 @@ if [ $# -ge 2 ];then
 	project_root=$2
 fi
 python3 -m cxxheaderparser --mode=json "$header_file_path" > parsed_header.json
-./parse_json.py parsed_header.json "$project_root"
+./tools/parse_json.py parsed_header.json "$project_root"
